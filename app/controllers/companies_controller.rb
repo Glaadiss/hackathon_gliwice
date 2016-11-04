27ca44bc05502d815848f868f1ea50216e39c7f7
Glaadiss <bartekgladys@gmail.com>
2016-11-05 00:00:56 +0100

    add relations

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -8,9 +8,25 @@ class CompaniesController < ApplicationController
   def new
     @company = Company.new
   end
+  
+  def create
+    @company = Company.new(companies_params)
+    if @company.save
+      redirect_to @company
+    else
+      render 'new'
+    end
+  end
 
   def show
+
     @recrutations = @company.recrutations
+  end
+
+  private
+
+  def companies_params
+    params.require(:company).permit(:name, :nip, :map, :address)
   end
 
   def find_company
