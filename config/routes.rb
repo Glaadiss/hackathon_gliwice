4e1c9ca463bba0e6d1f0668f9b49ce5801475ed5
Glaadiss <bartekgladys@gmail.com>
2016-11-05 13:12:23 +0100

    add chat

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,6 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "registrations" }
   resources :jobs
   resources :users, only: [:show, :index]
+  mount ActionCable.server, at: '/cable'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
