8a6a2d501636826e4755a2ad6184fb1c6a19121f
marcinrzepiel <marcinrzepiel@wp.pl>
2016-11-05 06:14:36 +0100

    nie wiem co robie

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,8 @@ before_action :authenticate_user!
 
   def show
     @user = current_user
-    @companies = Recrutation.joins(:notifications).where(notifications: {user_id: current_user.id}).distinct
+    @notifications = current_user.notifications.distinct
+
   end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def status(status)
+    if status == 0
+      content_tag "Oczekujący", "Oczekujący", class: 'btn btn-default btn-xs'
+    elsif status == 1
+      content_tag "Zaakceptowany", "Zaakceptowany", class: 'btn btn-success btn-xs'
+    elsif status == 2
+      content_tag "Odrzucony", "Odrzucony", class: 'btn btn-danger btn-xs'
+    end
+  end
 end
