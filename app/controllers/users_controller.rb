eb37fece2c015e7a1e619975572a1a99eba5d7bf
marcinrzepiel <marcinrzepiel@wp.pl>
2016-11-05 05:39:23 +0100

    nie wiem co robie

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ before_action :authenticate_user!
 
   def show
     @user = current_user
+    @companies = Recrutation.joins(:notifications).where(notifications: {user_id: current_user.id}).distinct
   end
 
 end
