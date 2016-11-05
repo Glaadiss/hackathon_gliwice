801bdd367c253a1a04ee8151cbee2fa31452aa66
marcinrzepiel <marcinrzepiel@wp.pl>
2016-11-05 03:36:47 +0100

    Front xd

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,6 @@ Rails.application.routes.draw do
   resources :messages
   devise_for :users, controllers: { registrations: "registrations" }
   resources :jobs
+  resources :users, only: [:show, :index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
