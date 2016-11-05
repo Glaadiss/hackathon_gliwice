c586bb32536de2422d77c9b5d51a0853b0583d32
marcinrzepiel <marcinrzepiel@wp.pl>
2016-11-05 16:13:37 +0100

    Wreszcie koniec

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-['Ruby dev', 'Php dev', 'JS DEV'].each {|d| Job.create(name: d)}
-Company.create( name: 'Starred', nip: '32131', map: '12331 123123', address: 'ul janusza', description: 'qweew')
-Recrutation.create!( title: 'cos tam co', dependency: ' qwe qe', job_id: Job.first.id, company_id: Company.first.id )
-User.create(email: 'qwe@gmail.com', firstname:'qwe', lastname: 'zaq', role: 0, password: '123456' )
-User.create(email: 'ade@gmail.com', firstname:'qwe', lastname: 'zaq', role: 1, password: '123456', company_id: Company.first.id )
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
